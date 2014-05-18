f379df5f738af76464405489a4bfcb6f580c1b14
Vikram Jambulapati <vikjam@gmail.com>
2014-05-17 22:30:41 -0400

    parallel process

--- 04 Instrumental Variables in Action/Figure 4-6-1.r
+++ 04 Instrumental Variables in Action/Figure 4-6-1.r
@@ -7,13 +7,15 @@
 library(AER)
 library(MASS)
 library(ivpack)
+library(parallel)
 
-nsims = 10000
-set.seed(42)
+nsims = 10
+set.seed(42, "L'Ecuyer")
 
-estimateBeta <- function() {
+estimateBeta <- function(...) {
     # Store coefficients
-    COEFS <- matrix(, 1, 3)
+    COEFS <- rep(NA, 3)
+    names(COEFS) <- c("ols", "tsls", "liml")
 
     # Set parameters
     Sigma  = matrix(c(1, 0.8, 0.8, 1), 2, 2)
@@ -28,15 +30,19 @@ estimateBeta <- function() {
 
     # OLS
     OLS <- lm(y ~ x)
-    COEFS[1, 1] <- summary(OLS)$coefficients[1, 1]
+    COEFS[1] <- summary(OLS)$coefficients[1, 1]
 
     # 2SLS
     TSLS <- ivreg(y ~ x, ~ Z)
-    COEFS[1, 2] <- summary(TSLS)$coefficients[1, 1]
+    COEFS[2] <- summary(TSLS)$coefficients[1, 1]
 
     return(COEFS)
 }
 
-ESTB <- sapply(1:nsims, function(x) estimateBeta())
+# Run simulations
+# SIMBETAS <- simplify2array(mclapply(1:nsims, estimateBeta))
+# print(SIMBETAS)
+
+# ESTB <- sapply(1:nsims, estimateBeta)
 
 # End of script
