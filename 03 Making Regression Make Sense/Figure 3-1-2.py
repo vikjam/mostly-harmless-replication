f988a8ac442db0ce2e9905f0a5c1726df6368c57
Vikram Jambulapati <vikjam@gmail.com>
2014-08-20 16:29:19 -0400

    start figure

--- 03 Making Regression Make Sense/Figure 3-1-2.py
+++ 03 Making Regression Make Sense/Figure 3-1-2.py
@@ -3,15 +3,15 @@ import urllib
 import zipfile
 import pandas as pd
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 # Download data and unzip the data
-# urllib.urlretrieve('http://economics.mit.edu/files/397', 'asciiqob.zip')
+urllib.urlretrieve('http://economics.mit.edu/files/397', 'asciiqob.zip')
 with zipfile.ZipFile('asciiqob.zip', "r") as z:
-    z.extractall()
+   z.extractall()
 
 # Read the data into a pandas dataframe
-pums = pd.read_csv('asciiqob.txt', 
-	               index_col        = 0,
+pums = pd.read_csv('asciiqob.txt',
 	               header           = None,
 	               delim_whitespace = True)
 pums.columns = ['lwklywge', 'educ', 'yob', 'qob', 'pob']
@@ -24,6 +24,14 @@ X = sm.add_constant(X)
 # Save coefficient on education
 model      = sm.OLS(y, X)
 results    = model.fit()
-educ_coefs = results.params[1]
+educ_coef  = results.params[1]
+intercept  = results.params[0]
+
+# Calculate means by educ attainment
+groupbyeduc = pums.groupby('educ')
+educ_means  = groupbyeduc['lwklywge'].mean()
+
+# Plot
+
 
 # End of script
