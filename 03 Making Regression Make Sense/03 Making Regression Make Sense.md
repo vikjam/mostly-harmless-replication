da711851dce1e2f1a92cb211663e351389c114bf
vikjam <vikjam@gmail.com>
2015-03-08 21:47:32 -0400

    add Julia plot

--- 03 Making Regression Make Sense/03 Making Regression Make Sense.md
+++ 03 Making Regression Make Sense/03 Making Regression Make Sense.md
@@ -1,3 +1,6 @@
 # 03 Making Regression Make Sense
 ## 3.4 Regression Details
 
+### Figure 3-1-2
+
+![Figure 3-1-2 in Julia](Figure 3-1-2-Juila.svg)
--- 03 Making Regression Make Sense/Figure 3-1-2.jl
+++ 03 Making Regression Make Sense/Figure 3-1-2.jl
@@ -1,6 +1,8 @@
 # Load packages
 using DataFrames
-using Gzip
+using GZip
+using Gadfly
+using GLM
 
 # Download the data
 download("http://economics.mit.edu/files/397", "asciiqob.zip")
@@ -9,7 +11,22 @@ download("http://economics.mit.edu/files/397", "asciiqob.zip")
 pums = readtable("asciiqob.txt",
                  header    = false,
                  separator = ' ')
-
 names!(pums, [:lwklywge, :educ, :yob, :qob, :pob])
+
+# Run OLS and save predicted values
+OLS = glm(lwklywge ~ educ, pums, Normal(), IdentityLink())
+pums[:predicted] = predict(OLS)
+
+# Aggregate into means for figure
+means = aggregate(pums, :educ, [mean])
+
+# Plot figure and export figure using Gadfly
+figure = plot(means,
+              layer(x = "educ", y = "predicted_mean", Geom.line, Theme(default_color = color("green"))),
+              layer(x = "educ", y = "lwklywge_mean", Geom.line, Geom.point),
+              Guide.xlabel("Years of completed education"),
+              Guide.ylabel("Log weekly earnings, \$2003"))
+
+draw(SVG("Figure 3-1-2-Julia.png", 5inch, 5inch), figure)
 
 # End of script
