c1a0f7698dd740a65074d6b9d18c7ca0e05f237d
vikjam <vikjam@gmail.com>
2015-05-13 23:50:27 -0400

    init table

---
+++ 08 Nonstandard Standard Error Issues/Table 8-1-1.jl
@@ -0,0 +1,19 @@
+# Julia code for Table 8-1-1                #
+# Required packages                         #
+# - DataFrames: data manipulation / storage #
+# - GLM: regression                         #
+using DataFrames
+using GLM
+
+# Set seed
+srand(123)
+
+function generateHC(sigma)
+    # Set parameters of the simulation
+    N   = 30
+    r   = 0.9
+    N_1 = r * 30
+
+    # Generate simulation data
+
+end
