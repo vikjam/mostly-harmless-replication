066b893083c128e4d539ee51936ae48cbd371580
vikjam <vikjam@gmail.com>
2015-06-13 20:28:57 -0400

    add regression fit

--- 06 Getting a Little Jumpy/Figure 6-1-1.py
+++ 06 Getting a Little Jumpy/Figure 6-1-1.py
@@ -21,9 +21,13 @@ y_linear  = x + (x > 0.5) * 0.25 + np.random.normal(0, 0.1, nobs)
 y_nonlin  = 0.5 * np.sin(6 * (x - 0.5)) + 0.5 + (x > 0.5) + np.random.normal(0, 0.1, nobs)
 y_mistake = 1 / (1 + np.exp(-25 * (x - 0.5))) + np.random.normal(0, 0.1, nobs)
 
+# Get fitted lines
+b1, b0 = np.polyfit(x, y_linear, 1)
+
 # Plot figures
 plt.figure()
 plt.scatter(x, y_linear, edgecolors = 'none')
+plt.plot(x, b0 + b1 * x)
 plt.axvline(0.5)
 plt.savefig('Figure 6-1-1-Python.pdf')
 
