b3ae98162a80eef3835e60a4a250deb7681b9fda
vikjam <vikjam@gmail.com>
2015-06-14 08:14:23 -0400

    Finish figure

--- 06 Getting a Little Jumpy/Figure 6-1-1.py
+++ 06 Getting a Little Jumpy/Figure 6-1-1.py
@@ -21,13 +21,18 @@ y_linear  = x + (x > 0.5) * 0.25 + np.random.normal(0, 0.1, nobs)
 y_nonlin  = 0.5 * np.sin(6 * (x - 0.5)) + 0.5 + (x > 0.5) + np.random.normal(0, 0.1, nobs)
 y_mistake = 1 / (1 + np.exp(-25 * (x - 0.5))) + np.random.normal(0, 0.1, nobs)
 
-# Get fitted lines
-b1, b0 = np.polyfit(x, y_linear, 1)
+# Fit lines
+coef_y_linear_0 = np.polyfit(x[0.5 >= x], y_linear[0.5 >= x], 1)
+fit_y_linear_0  = np.polyval(coef_y_linear_0, x[0.5 >= x])
+
+coef_y_linear_1 = np.polyfit(x[x > 0.5], y_linear[x > 0.5], 1)
+fit_y_linear_1  = np.polyval(coef_y_linear_1, x[x > 0.5])
 
 # Plot figures
 plt.figure()
 plt.scatter(x, y_linear, edgecolors = 'none')
-plt.plot(x, b0 + b1 * x)
+plt.plot(x[0.5 >= x], fit_y_linear_0)
+plt.plot(x[x > 0.5], fit_y_linear_1)
 plt.axvline(0.5)
 plt.savefig('Figure 6-1-1-Python.pdf')
 
