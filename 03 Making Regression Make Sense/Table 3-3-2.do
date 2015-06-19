471a8e8d5c69ee3e6e70b6878fc82e87972cce14
vikjam <vikjam@gmail.com>
2015-06-18 23:29:43 -0400

    Add propensity trimmed columns

--- 03 Making Regression Make Sense/Table 3-3-2.do
+++ 03 Making Regression Make Sense/Table 3-3-2.do
@@ -6,28 +6,41 @@ capture version 13
 
 /* Stata code for Table 3.3.2*/
 
-log using "Table 3-3-2-Stata.txt", name(table030302) text replace
-
-* /* Download data */
-* shell /usr/local/bin/wget -O cps1re74.dta http://economics.mit.edu/files/3824
-* shell /usr/local/bin/wget -O cps3re74.dta http://economics.mit.edu/files/3825
-* shell /usr/local/bin/wget -O nswre74.dta http://economics.mit.edu/files/3828
+/* Download data */
+* shell curl -o nswre74.dta  http://economics.mit.edu/files/3828
+* shell curl -o cps1re74.dta http://economics.mit.edu/files/3824
+* shell curl -o cps3re74.dta http://economics.mit.edu/files/3825
 
 /* Columns 2 and 1 */
 use "nswre74.dta", clear
 tabstat age ed black hisp married nodeg, by(treat) format(%9.2f)
-tabstat re74 re75, by(treat) format(%9.0f)
+tabstat re74 re75, by(treat) format(%9.0f) stats(mean count)
 
 /* Column 3 */
 use "cps1re74.dta", clear
 tabstat age ed black hisp married nodeg if treat == 0, format(%9.2f)
-tabstat re74 re75 if treat == 0, format(%9.0f)
+tabstat re74 re75 if treat == 0, format(%9.0f) stats(mean count)
+
+/* Column 5 */
+probit treat age age2 ed black hisp married nodeg re74 re75
+predict p_score, pr
+keep if p_score > 0.1 & p_score < 0.9
+
+tabstat age ed black hisp married nodeg if treat == 0, format(%9.2f)
+tabstat re74 re75 if treat == 0, format(%9.0f) stats(mean count)
 
 /* Column 4 */
 use "cps3re74.dta", clear
 tabstat age ed black hisp married nodeg if treat == 0, format(%9.2f)
-tabstat re74 re75 if treat == 0, format(%9.0f)
+tabstat re74 re75 if treat == 0, format(%9.0f) stats(mean count)
 
-log close table030302
+/* Column 6 */
+probit treat age age2 ed black hisp married nodeg re74 re75
+predict p_score, pr
+keep if p_score > 0.1 & p_score < 0.9
+
+tabstat age ed black hisp married nodeg if treat == 0, format(%9.2f)
+tabstat re74 re75 if treat == 0, format(%9.0f) stats(mean count)
+
 /* End of script */
 exit
--- 03 Making Regression Make Sense/Table 3-3-2.r
+++ 03 Making Regression Make Sense/Table 3-3-2.r
@@ -1,17 +1,75 @@
 # R code for Table 3-3-2     #
 # Required packages          #
 # - foreign: read .dta files #
+# - knitr: print markdown    #
 
 library(foreign)
+library(knitr)
 
 # Download the files
-download.file("http://economics.mit.edu/files/3824", "cps1re74.dta")
-download.file("http://economics.mit.edu/files/3825", "cps3re74.dta")
-download.file("http://economics.mit.edu/files/3828", "nswre74.dta")
+# download.file("http://economics.mit.edu/files/3828", "nswre74.dta")
+# download.file("http://economics.mit.edu/files/3824", "cps1re74.dta")
+# download.file("http://economics.mit.edu/files/3825", "cps3re74.dta")
 
 # Read the Stata files into R
+nswre74  <- read.dta("nswre74.dta")
 cps1re74 <- read.dta("cps1re74.dta")
 cps3re74 <- read.dta("cps3re74.dta")
-nswre74  <- read.dta("nswre74.dta")
 
+# Function to create propensity trimmed data
+propensity.trim <- function(dataset) {
+    # Specify control formulas
+    controls <- c("age", "age", "ed", "black", "hisp", "nodeg", "married", "re74", "re75")
+    # Paste together probit specification
+    spec <- paste("treat", paste(controls, collapse = " + "), sep = " ~ ")
+    # Run probit
+    probit <- glm(as.formula(spec), family = binomial(link = "probit"), data = dataset)
+    # Predict probability of treatment
+    pscore <- predict(probit, type = "response")
+    # Return data set within range
+    dataset[which(pscore > 0.1 & pscore < 0.9), ]
+}
 
+# Propensity trim data
+cps1re74.ptrim <- propensity.trim(cps1re74)
+cps3re74.ptrim <- propensity.trim(cps3re74)
+
+# Create function for summary statistics
+summarize <- function(dataset, treat) {
+    # Variables to summarize
+    summary.variables <- c("age", "ed", "black", "hisp", "nodeg", "married", "re74", "re75")
+    # Calculate mean, removing missing
+    summary.means <- sapply(dataset[treat, summary.variables], mean, na.rm = TRUE)
+    summary.count <- sum(treat)
+    c(summary.means, count = summary.count)
+}
+
+# Summarize data
+nswre74.treat.stats   <- summarize(nswre74, nswre74$treat == 1)
+nswre74.control.stats <- summarize(nswre74, nswre74$treat == 0)
+cps1re74.stats        <- summarize(cps1re74, cps1re74$treat == 0)
+cps3re74.stats        <- summarize(cps3re74, cps3re74$treat == 0)
+cps1re74.ptrim.stats  <- summarize(cps1re74.ptrim, cps1re74.ptrim$treat == 0)
+cps3re74.ptrim.stats  <- summarize(cps3re74.ptrim, cps3re74.ptrim$treat == 0)
+
+summary.stats <- rbind(nswre74.treat.stats,
+                       nswre74.control.stats,
+                       cps1re74.stats,
+                       cps3re74.stats,
+                       cps1re74.ptrim.stats,
+                       cps3re74.ptrim.stats)
+summary.stats <- t(summary.stats)
+
+colnames(summary.stats)  <- c("NSW Treat", "NSW Control",
+                              "Full CPS-1", "Full CPS-3",
+                              "P-score CPS-1", "P-score CPS-3")
+row.names(summary.stats) <- c("Age",
+                              "Years of Schooling",
+                              "Hispanic",
+                              "Dropout",
+                              "Married",
+                              "1974 earnings",
+                              "1975 earnings",
+                              "Number of Obs.")
+
+kable(summary.stats)
