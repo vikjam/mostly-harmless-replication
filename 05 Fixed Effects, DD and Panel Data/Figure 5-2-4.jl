71a68701ffa352b0ebd11ccadb60773033cc3eb9
vikjam <vikjam@gmail.com>
2016-02-08 18:42:56 -0500

    Typos

--- 05 Fixed Effects, DD and Panel Data/Figure 5-2-4.jl
+++ 05 Fixed Effects, DD and Panel Data/Figure 5-2-4.jl
@@ -3,10 +3,10 @@ using DataFrames
 using DataRead
 
 # Download the data and unzip it
-download('http://economics.mit.edu/~dautor/outsourcingatwill_table7.zip', 'outsourcingatwill_table7.zip')
+download("http://economics.mit.edu/~dautor/outsourcingatwill_table7.zip", "outsourcingatwill_table7.zip")
 run(`unzip outsourcingatwill_table7.zip`)
 
 # Import data
-autor = pd.read_stata('table7/autor-jole-2003.dta')
+autor = read_dta("table7/autor-jole-2003.dta")
 
 # End of script
