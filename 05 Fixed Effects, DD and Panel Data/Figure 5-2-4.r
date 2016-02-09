213180eabe8b269703048520c9e3017e4cdfdb1b
Vikram Jambulapati <vikjam@gmail.com>
2016-02-09 14:33:51 -0500

    Add regression, std error adj.

--- 05 Fixed Effects, DD and Panel Data/Figure 5-2-4.r
+++ 05 Fixed Effects, DD and Panel Data/Figure 5-2-4.r
@@ -1,7 +1,9 @@
-# R code for Figure 4-1-1         #
-# Required packages               #
-# foreign: read Stata .dta files  #
+# R code for Figure 4-1-1             #
+# Required packages                   #
+# foreign: read Stata .dta files      #
 library(foreign)
+library(plm)
+library(lmtest)
 
 # Download the data and unzip it
 download.file('http://economics.mit.edu/~dautor/outsourcingatwill_table7.zip', 'outsourcingatwill_table7.zip')
@@ -23,7 +25,7 @@ autor$bizemp <- autor$svcemp + autor$peremp
 autor$lnbiz  <- log(autor$biz)
 
 # Restrict sample
-autor <- autor[which(autor$year >= 79 & autor$year <= 85), ]
+autor <- autor[which(autor$year >= 79 & autor$year <= 95), ]
 autor <- autor[which(autor$state != 98), ]
 
 # State dummies, year dummies, and state*time trends
@@ -45,7 +47,30 @@ autor$married <- autor$marfem + autor$marmale
 autor$unmem[79 >= autor$year & autor$year >= 81] <- NA
 autor$unmem                                      <- autor$unmem * 100
 
+# Make into a panel data set
+p.df <- pdata.frame(autor, index = c('state', 'year'))
+
 # Diff-in-diff regression
-did <- lm(lnths ~ factor(year) + factor(state) + factor(state) * t, data = autor)
+pdid <- plm(lnths ~ lnemp    + admico_2 + admico_1 + admico0 + admico1 + admico2 + admico3 + mico4    +
+                    admppa_2 + admppa_1 + admppa0  + admppa1 + admppa2 + admppa3 + mppa4   + admgfa_2 +
+                    admgfa_1 + admgfa0  + admgfa1  + admgfa2 + admgfa3 + mgfa4   +
+                    factor(year) + factor(state) + factor(state) * t,
+                    data  = p.df,
+                    model = 'pooling')
+
+did <- lm(lnths ~ lnemp    + admico_2 + admico_1 + admico0 + admico1 + admico2 + admico3 + mico4    +
+                   admppa_2 + admppa_1 + admppa0  + admppa1 + admppa2 + admppa3 + mppa4   + admgfa_2 +
+                   admgfa_1 + admgfa0  + admgfa1  + admgfa2 + admgfa3 + mgfa4   +
+                   factor(year) + factor(state) + factor(state) * t,
+                   data  = autor)
+
+# Compute Stata like degrees of freedom adjustment
+G   <- length(unique(p.df$state))
+N   <- length(p.df$state)
+DFA <- (G / (G - 1)) * (N - 1) / did$df.residual
+ 
+# Calculate cluster VCE
+did_vcov <- DFA * vcovHC(did, type = "HC1", cluster = "group", adjust = TRUE)
+coeftest(pdid, vcov = did_vcov)
 
 # End of script
