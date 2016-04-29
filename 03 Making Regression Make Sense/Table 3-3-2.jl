74e3dd816cfa40d26cb4448b3ef4953d8ad540d2
vikjam <vikjam@gmail.com>
2016-04-29 08:32:31 -0400

    New Figure 6-1-1-Stata.png, progress on Table 3-3-2.jl

--- 03 Making Regression Make Sense/Table 3-3-2.jl
+++ 03 Making Regression Make Sense/Table 3-3-2.jl
@@ -7,9 +7,17 @@ download('http://economics.mit.edu/files/3828', 'nswre74.dta')
 download('http://economics.mit.edu/files/3824', 'cps1re74.dta')
 download('http://economics.mit.edu/files/3825', 'cps3re74.dta')
 
-# Read the Stata files into Python
-nswre74  = read_dta("nswre74.dta")
-cps1re74 = read_dta("cps1re74.dta")
-cps3re74 = read_dta("cps3re74.dta")
+# # Read the Stata files into Julia
+# nswre74  = read_dta("nswre74.dta")
+# cps1re74 = read_dta("cps1re74.dta")
+# cps3re74 = read_dta("cps3re74.dta")
+
+# Read the CSV files into Julia
+nswre74  = readtable("nswre74.csv")
+cps1re74 = readtable("cps1re74.csv")
+cps3re74 = readtable("cps3re74.csv")
+
+summary_vars = [:age, :ed, :black], :hisp, :nodeg, :married, :re74, :re75]
+nswre74_stat = aggregate(nswre74, summary_vars, [mean])
 
 # End of script
