1ada1ebeb836de8651d2b214618cefd774fcd154
vikjam <vikjam@gmail.com>
2016-05-10 15:57:16 -0400

    Clean-up, add labels

--- 04 Instrumental Variables in Action/Figure 4-1-1.py
+++ 04 Instrumental Variables in Action/Figure 4-1-1.py
@@ -9,6 +9,7 @@ import urllib.request
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 # Download data and unzip the data
 # urllib.request.urlretrieve('http://economics.mit.edu/files/397', 'asciiqob.zip')
@@ -27,31 +28,36 @@ birth_means  = groupbybirth['lwklywge', 'educ'].mean()
 
 # Create function to plot figures
 def plot_qob(yvar, ax, title, ylabel):
-    values = birth_means['educ'].values
-    ax.plot(values)
-    ax.title('A. Average education by quarter of birth (first stage)')
+    values = yvar.values
+    ax.plot(values, marker = 'o')
+
+    for i, y in enumerate(yvar):
+        ax.annotate(yvar.index.get_level_values('qob')[i],
+                    (i, y),
+                    xytext = (-5, 5),
+                    textcoords = 'offset points')
+
+    ax.set_xticks(range(0, len(yvar), 4))
+    ax.set_xticklabels(yvar.index.get_level_values('yob')[1::4])
+    ax.set_title(title)
     ax.set_ylabel(ylabel)
-    ax.set_xlabel("Quarter of birth")
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax.set_xlabel("Year of birth")
+    ax.margins(0.1)
 
-fig, ax = plt.subplots()
-plot_qob(birth_means['educ'], ax, 'A. Average education by quarter of birth (first stage)', 'Years of education')
+fig, (ax1, ax2) = plt.subplots(2, sharex = True)
 
-# Panel A
-plt.subplot(2, 1, 1)
-plt.title("A. Average education by quarter of birth (first stage)")
-plt.set_ylabel("Years of education")
-birth_means.plot(y = 'educ', marker = 'o')
+plot_qob(yvar   = birth_means['educ'],
+         ax     = ax1,
+         title  = 'A. Average education by quarter of birth (first stage)',
+         ylabel = 'Years of education')
 
-# Panel B
-plt.subplot(2, 1, 2)
-plt.title("B. Average weekly wage by quarter of birth (reduced form)")
-plt.set_ylabel("Log weekly earnings")
-birth_means.plot(y = 'lwklywge', marker = 'o')
+plot_qob(yvar   = birth_means['lwklywge'],
+         ax     = ax2,
+         title  = 'B. Average weekly wage by quarter of birth (reduced form)',
+         ylabel = 'Log weekly earnings')
 
-# Format overall figure and export to PDF
-plt.legend().set_visible(False)
-plt.tight_layout() # http://matplotlib.org/users/tight_layout_guide.html
-plt.savefig('Figure 4-1-1-Python.pdf', format = 'pdf')
-plt.close('all')
+fig.tight_layout()
+fig.savefig('Figure 4-1-1-Python.pdf', format = 'pdf')
 
 # End of file
