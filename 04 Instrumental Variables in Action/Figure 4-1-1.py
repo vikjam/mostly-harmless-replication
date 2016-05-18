751e95e34fe0fe680c09d2a4e9f8ee2abf9dc218
vikjam <vikjam@gmail.com>
2016-05-18 12:38:11 -0400

    Hollow quarter 1, uncomment download

--- 04 Instrumental Variables in Action/Figure 4-1-1.py
+++ 04 Instrumental Variables in Action/Figure 4-1-1.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
 # Download data and unzip the data
-# urllib.request.urlretrieve('http://economics.mit.edu/files/397', 'asciiqob.zip')
-# with zipfile.ZipFile('asciiqob.zip', "r") as z:
-#    z.extractall()
+urllib.request.urlretrieve('http://economics.mit.edu/files/397', 'asciiqob.zip')
+with zipfile.ZipFile('asciiqob.zip', "r") as z:
+   z.extractall()
 
 # Read the data into a pandas dataframe
 pums         = pd.read_csv('asciiqob.txt',
@@ -29,13 +29,18 @@ birth_means  = groupbybirth['lwklywge', 'educ'].mean()
 # Create function to plot figures
 def plot_qob(yvar, ax, title, ylabel):
     values = yvar.values
-    ax.plot(values, marker = 'o')
+    ax.plot(values, color = 'k')
 
     for i, y in enumerate(yvar):
-        ax.annotate(yvar.index.get_level_values('qob')[i],
+        qob = yvar.index.get_level_values('qob')[i]
+        ax.annotate(qob,
                     (i, y),
                     xytext = (-5, 5),
                     textcoords = 'offset points')
+        if qob == 1:
+            ax.scatter(i, y, marker = 's', facecolors = 'none', edgecolors = 'k')
+        else:
+            ax.scatter(i, y, marker = 's', color = 'k')
 
     ax.set_xticks(range(0, len(yvar), 4))
     ax.set_xticklabels(yvar.index.get_level_values('yob')[1::4])
