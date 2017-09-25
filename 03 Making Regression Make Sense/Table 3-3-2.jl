22d41f26f3efeca84bcaf48892a3aa63a8e362ec
Shunsuke Hori <hori0518@gmail.com>
2017-09-25 11:04:12 -0700

    make the code work

--- 03 Making Regression Make Sense/Table 3-3-2.jl
+++ 03 Making Regression Make Sense/Table 3-3-2.jl
@@ -1,36 +1,31 @@
 # Load packages
 using DataFrames
-# using DataRead
+using ReadStat
 using GLM
 
 # # Download the data
-# download('http://economics.mit.edu/files/3828', 'nswre74.dta')
-# download('http://economics.mit.edu/files/3824', 'cps1re74.dta')
-# download('http://economics.mit.edu/files/3825', 'cps3re74.dta')
+download("http://economics.mit.edu/files/3828", "nswre74.dta")
+download("http://economics.mit.edu/files/3824", "cps1re74.dta")
+download("http://economics.mit.edu/files/3825", "cps3re74.dta")
 
 # # Read the Stata files into Julia
-# nswre74  = read_dta("nswre74.dta")
-# cps1re74 = read_dta("cps1re74.dta")
-# cps3re74 = read_dta("cps3re74.dta")
-
-# Read the CSV files into Julia
-nswre74  = readtable("nswre74.csv")
-cps1re74 = readtable("cps1re74.csv")
-cps3re74 = readtable("cps3re74.csv")
+nswre74  = read_dta("nswre74.dta")
+cps1re74 = read_dta("cps1re74.dta")
+cps3re74 = read_dta("cps3re74.dta")
 
 summary_vars = [:age, :ed, :black, :hisp, :nodeg, :married, :re74, :re75]
 nswre74_stat = aggregate(nswre74, summary_vars, [mean])
 
 # Calculate propensity scores
-probit = glm(treat ~ age + age2 + ed + black + hisp + 
-	                 nodeg + married + re74 + re75,
+probit = glm(@formula(treat ~ age + age2 + ed + black + hisp +
+	                 nodeg + married + re74 + re75),
 	                 cps1re74,
 	         Binomial(),
 	         ProbitLink())
 cps1re74[:pscore] = predict(probit)
 
-probit = glm(treat ~ age + age2 + ed + black + hisp +
-	                 nodeg + married + re74 + re75,
+probit = glm(@formula(treat ~ age + age2 + ed + black + hisp +
+	                 nodeg + married + re74 + re75),
 	                 cps3re74,
 	         Binomial(),
 	         ProbitLink())
